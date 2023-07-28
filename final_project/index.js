1068d5f57653466f5006fa2761fc23d1c4cd6efe
Tiago Pereira de Medeiros <117854547+Alkateya@users.noreply.github.com>
2023-07-28 16:35:37 -0300

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,48 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
-const genl_routes = require('./router/general.js').general;
-
+const customer_routes = require('./router/auth_users.js');
+const genl_routes = require('./router/general.js');
 const app = express();
+require('dotenv').config();
+
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({
+  secret: process.env.JWT_SECRET || "default-secret",
+  resave: true, 
+  saveUninitialized: true
+}));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+app.use("/customer/auth", function auth(req, res, next) {
+    const token = req.headers['x-access-token'];
+    if (!token) return res.status(403).send({ auth: false, message: 'No token provided.' });
+  
+    jwt.verify(token, process.env.JWT_SECRET || "default-secret", function(err, decoded) {
+      if (err) return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+      // se tudo estiver ok, salva no request para uso posterior
+      req.userId = decoded.id;
+      next();
+    });
+  });
+  
+
  
-const PORT =5000;
+const PORT = process.env.PORT || 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
+
+
+
+process.on('uncaughtException', function(err) {
+    console.log('Caught exception: ' + err);
+  });
+  
+  process.on('unhandledRejection', function(err) {
+    console.log('Caught rejection: ' + err);
+  });
+  
